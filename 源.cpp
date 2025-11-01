3a35fd2f9c2cb047e4bef94b3bf7078168199b63
帝鸿 <hongmong20221953@outlook.com>
2025-11-02 00:12:18 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -2,54 +2,54 @@
 #include<stdio.h>
 
 
-//int main()
-//{
-//	int num = -1;
-//	int i = 0;
-//	int count = 0;
-//	for (i = 0; i < 32; i++)
-//	{
-//		if (num & (1 << i))
-//			count++;
-//	}
-//	printf("二进制中1的个数 = %d\n", count);
-//	return 0;
-//}
+int main()
+{
+	int num = -1;
+	int i = 0;
+	int count = 0;
+	for (i = 0; i < 32; i++)
+	{
+		if (num & (1 << i))
+			count++;
+	}
+	printf("二进制中1的个数 = %d\n", count);
+	return 0;
+}
 
 
 
-//int main()
-//{
-//	int num = 1;
-//	int i = 0;
-//	int count = 0;
-//	while (num)
-//	{
-//		count++;
-//		num = num & (num - 1);
-//	}
-//	printf("二进制中1的个数 = %d\n", count);
-//	return 0;
-//}
+int main()
+{
+	int num = 1;
+	int i = 0;
+	int count = 0;
+	while (num)
+	{
+		count++;
+		num = num & (num - 1);
+	}
+	printf("二进制中1的个数 = %d\n", count);
+	return 0;
+}
 
 
 
-//int main()
-//{
-//	int n = 0;
-//	scanf("%d", &n);
-//	int count = 0;
-//	while (n)
-//	{
-//		if (n % 2 == 1)
-//			count++;
-//		n /= 2;
-//	}
-//	printf("%d\n", count);
-//	return 0;
-//}
-////-1算的是错的，为0
-////想挽救一下unsigned int n = 0;就行
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int count = 0;
+	while (n)
+	{
+		if (n % 2 == 1)
+			count++;
+		n /= 2;
+	}
+	printf("%d\n", count);
+	return 0;
+}
+//-1算的是错的，为0
+//想挽救一下unsigned int n = 0;就行
 
 
 

5de2dbbdf319de7aa13ba17af440165c78a78d8c
帝鸿 <225365686+SFEW888@users.noreply.github.com>
2025-11-02 22:25:27 +0800

    Create 源.cpp

---
+++ test_25_11_topic_1/test_25_11_topic_1/源.cpp
@@ -0,0 +1,162 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+//int main() {
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    int d = 0;
+//    scanf("%d%d", &a, &b);
+//    c = a / b;
+//    d = a % b;
+//    printf("%d %d ", c, d);
+//
+//    return 0;
+//}
+
+
+
+
+//int main() {
+//    int a = 0;
+//    scanf("%d", &a);
+//    if (a >= 140)
+//    {
+//        printf("Genius");
+//    }
+//    return 0;
+//}
+
+
+
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    while (scanf("%d%d", &a, &b) != EOF)
+//    {
+//        if (a == b)
+//            printf("%d=%d\n", a, b);
+//        else if (a > b)
+//            printf("%d>%d\n", a, b);
+//        else
+//            printf("%d<%d\n", a, b);
+//    }
+//    return 0;
+//}
+
+
+
+
+//int main() {
+//    int a = 0;
+//    scanf("%d", &a);
+//    if (a % 5 == 0)
+//        printf("YES");
+//    else
+//        printf("NO");
+//    return 0;
+//}
+
+
+
+
+//int main() {
+//    int a = 0;
+//    int b = 0;
+//    while (scanf("%d", &b) != EOF)
+//    {
+//        for (a = 1; a < b; a++)
+//        {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//
+//    return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	for (a = 1; a < 100; a++)
+//	{
+//		if (a % 3 == 0)
+//		{
+//			printf("%d ", a);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a > b && b > c)
+//	{
+//		printf("%d %d %d", a, b, c);
+//	}
+//	else if (a > c && c > b)
+//	{
+//		printf("%d %d %d", a, b, c);
+//	}
+//	else if (b > a && a > c)
+//	{
+//		printf("%d %d %d", b, a, c);
+//	}
+//	else if (b > c && c > a)
+//	{
+//		printf("%d %d %d", b, c, a);
+//	}
+//	else if (c > b && b > a)
+//	{
+//		printf("%d %d %d", c, b, a);
+//	}
+//	else
+//		printf("%d %d %d", c, a, b);
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	scanf("%d", &b);
+//	for (a = 1; a < b; a++)
+//	{
+//		if (a % 2 == 0)
+//		{
+//			int c = -(1 / a);
+//		}
+//		else
+//		{
+//			int d = 1 / a;
+//		}
+//		int e = c + d;
+//	}
+//	printf("%d\n", e);
+//	return 0;
+//}
+
+
+
+

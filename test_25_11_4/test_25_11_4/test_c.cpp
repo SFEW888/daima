3d948a1fd19f0acbb9b8128021d7415c65f602e2
帝鸿 <225365686+SFEW888@users.noreply.github.com>
2025-11-06 10:57:25 +0800

    Create test_c.cpp

---
+++ test_25_11_4/test_25_11_4/test_c.cpp
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+# include<stdio.h>
+//
+//struct student
+//{
+//	char name[20];
+//	int name;
+//	int sex
+//};
+//
+//
+//
+//
+//
+//int main()
+//{
+//
+//
+//
+//
+//
+//}
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//    //局部变量
+//    int a;
+//    char c;
+//    float f;
+//    double d;
+//
+//    cout << "int:" << a << endl;
+//    cout << "char:" << c << endl;
+//    cout << "float:" << f << endl;
+//    cout << "double:" << d << endl;
+//    return 0;
+//}
+
+
+
+//int a;
+//char c;
+//float f;
+//double d;
+//int main()
+//{
+//    cout << "int:" << a << endl;
+//    cout << "char:" << c << endl;
+//    cout << "float:" << f <<
+//        endl;
+//    cout << "double:" << d <<
+//        endl;
+//    return 0;
+//}
+
+
+
+
+
+//int main() {
+//    int n;
+//    scanf("%d", &n);
+//
+//    int odd_count = 0, even_count = 0;
+//
+//    for (int i = 0; i < n; i++) {
+//        int num;
+//        scanf("%d", &num);
+//
+//        if (num % 2 == 0) {
+//            even_count++;
+//        }
+//        else {
+//            odd_count++;
+//        }
+//    }
+//
+//    printf("%d %d\n", odd_count, even_count);
+//
+//    return 0;
+//}

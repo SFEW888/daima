99c4e8e987c18d79effb70c7d3db9d021c176c5d
帝鸿 <225365686+SFEW888@users.noreply.github.com>
2025-11-10 23:17:14 +0800

    Create test_25_11_10.c

---
+++ test_25_11_10/test_25_11_10/test_25_11_10.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main() {
+//	double a, b, c;
+//	double discriminant, num1, num2, realPart, imagPart;
+//	printf("请输入二次方程ax?+bx+c=0的系数a, b, c: ");
+//	scanf("%lf %lf %lf", &a, &b, &c);
+//	if (a == 0 && b == 0 && c == 0) {
+//		printf("方程有无数个根\n");
+//	}
+//	else if (a == 0 && b == 0 && c != 0) {
+//		printf("方程无根\n");
+//	}
+//	else if (a == 0 && b != 0) {
+//		num1 = -c / b;
+//		printf("方程只有一个实根: %.2f\n", num1);
+//	}
+//	else {
+//		discriminant = b * b - 4 * a * c;
+//		if (discriminant >= 0) {
+//			num1 = (-b + sqrt(discriminant)) / (2 * a);
+//			num2 = (-b - sqrt(discriminant)) / (2 * a);
+//			if (discriminant == 0) {
+//				printf("方程有两个相等的实根: %.2f\n", num1);
+//			}
+//			else {
+//				printf("方程有两个不相等的实根: %.2f 和 %.2f\n", num1, num2);
+//			}
+//		}
+//		else {
+//			realPart = -b / (2 * a);
+//			imagPart = sqrt(-discriminant) / (2 * a);
+//			printf("方程有两个虚根: %.2f+%.2fi 和 %.2f-%.2fi\n", realPart, imagPart, realPart, imagPart);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	int n = 10;
+//	char* pc = (char*)&n;
+//	int* pi = &n;
+//	printf("%p\n", &n);
+//	printf("%p\n", pc);
+//	printf("%p\n", pc + 1);
+//	printf("%p\n", pi);
+//	printf("%p\n", pi + 1);
+//	return 0;
+//}
+
+
+
+
+

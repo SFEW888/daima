1a2c518059b7dfa31f3da0729ca6680fb392a776
帝鸿 <225365686+SFEW888@users.noreply.github.com>
2025-11-13 23:28:03 +0800

    Create test_25_11_13.c

---
+++ test_25_11_13/test_25_11_13/test_25_11_13.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//void test(int arr[3][5], int a, int b)
+//{
+//	int i = 0;
+//	
+//	for (i = 0; i < a; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < b; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//
+//int main()
+//{
+//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+//	test(arr, 3, 5);
+//	return 0;
+//}
